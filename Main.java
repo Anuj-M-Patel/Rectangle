7176da408a7313190f7e5904cb2b498e6daf544c
Anuj-M-Patel <42977181+Anuj-M-Patel@users.noreply.github.com>
2018-10-02 13:35:10 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input length: ");
+		double length = input.nextDouble();
+		input.nextLine();
+		System.out.print("Input width: ");
+		double width = input.nextDouble();
+		input.nextLine();
+		
+		double area = length * width;
+		double perimeter = 2 * length + 2 * width;
+		double diagonal = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+		
+		System.out.printf("Area: %.2f", area);
+		System.out.printf("\nPerimeter: %.2f", perimeter);
+		System.out.printf("\nDiagonal: %.2f", diagonal);
+
+	}
+
+}
